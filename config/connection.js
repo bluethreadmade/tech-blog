086de59e3e6a841606156e5315566bc8cd54ddff
Megan McNeill <mcneim2@gmail.com>
2024-07-24 23:03:10 -0400

    updated config connection to work with render

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,28 @@
-const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'postgres',
-    }
-  );
+const Sequelize = require('sequelize');
+
+let sequelize;
+
+if (process.env.DB_URL) {
+    sequelize = new Sequelize(process.env.DB_URL, {
+        hooks: {
+            beforeDefine: function (columns, model) {
+              model.tableName = `${process.env.DB_NAME}_${model.name.singular}`;
+            },
+          },
+        });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+        host: 'localhost',
+        dialect: 'postgres',
+        }
+    );
+}
 
 module.exports = sequelize;
+
